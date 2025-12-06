c4f5383b5cd07d79c2c88a850c8cabc77952e1d4
SAUMYA GUPTA <saumyag1202@gmail.com>
2025-12-06 19:21:28 +0530

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,15 @@
 import express from "express";
 
 const app = express();
-
 app.use(express.json());
 app.post("/api/sort", (req, res) => {
-  const numbers = req.body.numbers;
+  const { numbers } = req.body;
 
   if (!Array.isArray(numbers)) {
     return res.status(400).json({ error: "enter 'numbers' array" });
   }
 
-  const sorted = numbers.sort((a, b) => b - a);
-  return res.json({ sorted });
-});
-
-app.get("/", (req, res) => {
-  res.send("Sort API is running. Use POST /api/sort");
+  res.json({ sorted: numbers.sort((a, b) => b - a) });
 });
 
 export default app;
