45a2e44a45ce521fd38a011b1e753a234d8720d8
SAUMYA GUPTA <saumyag1202@gmail.com>
2025-12-06 19:18:40 +0530

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,21 @@
-import express from 'express'
+import express from "express";
 
-const app = express()
+const app = express();
 
-app.use(express.json())
-app.post('/api/sort', (req, res) => {
-  const numbers = req.body.numbers
+app.use(express.json());
+app.post("/api/sort", (req, res) => {
+  const numbers = req.body.numbers;
 
   if (!Array.isArray(numbers)) {
-    return res.status(400).json({ error: "enter 'numbers' array" })
+    return res.status(400).json({ error: "enter 'numbers' array" });
   }
 
-  const sorted = numbers.sort((a, b) => b - a)
-  res.json({ sorted })
-})
+  const sorted = numbers.sort((a, b) => b - a);
+  return res.json({ sorted });
+});
 
-app.get('/', (req, res) => {
-  res.send("Sort API is running 🚀. POST JSON to /api/sort")
-})
+app.get("/", (req, res) => {
+  res.send("Sort API is running. Use POST /api/sort");
+});
 
-export default app
+export default app;
